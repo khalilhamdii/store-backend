374e2f2b5a3197627f06e48dcda363f836a1ac23
khalilhamdii <khaalil.hamdi@gmail.com>
2021-04-04 23:49:09 +0100

    Add mongoose and connect it to MongoDB Atlas

--- app.js
+++ app.js
@@ -1,7 +1,20 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const app = express();
+
+mongoose
+  .connect(
+    'mongodb+srv://khalilhamdiii:lQ8swrGpJZrIF7Jm@cluster0.31hme.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+  )
+  .then(() => {
+    console.log('Successfuly connected to MongoDB Atlas!');
+  })
+  .catch((error) => {
+    console.log('Unable to connect to MongoDB Atlas');
+    console.error(error);
+  });
 
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
