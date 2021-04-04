3238db2d4828089b722d47308aac482aa050b832
khalilhamdii <khaalil.hamdi@gmail.com>
2021-04-04 22:37:39 +0100

    Configure node server to handle errors

--- app.js
+++ app.js
@@ -1,5 +1,27 @@
 const express = require('express');
 
 const app = express();
+app.use('/api/stuff', (req, res, next) => {
+  const stuff = [
+    {
+      _id: 'aflkzhfa',
+      title: 'My first thing',
+      description: 'All of the info of my first thing',
+      imageUrl: '',
+      price: 4900,
+      userId: 'kjahzfakl',
+    },
+    {
+      _id: 'aflhfa',
+      title: 'My second thing',
+      description: 'All of the info of my Second thing',
+      imageUrl: '',
+      price: 1200,
+      userId: 'kjahakl',
+    },
+  ];
+
+  res.status(200).json(stuff);
+});
 
 module.exports = app;
