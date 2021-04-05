793af7e72d66e61c5a7de7990b8f235e6ccbeb05
khalilhamdii <khaalil.hamdi@gmail.com>
2021-04-05 01:25:51 +0100

    Add thingSchema and model + add post & get feature

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const Thing = require('./models/thing');
 
 const app = express();
 
@@ -32,33 +33,51 @@ app.use((req, res, next) => {
 app.use(bodyParser.json());
 
 app.post('/api/stuff', (req, res, next) => {
-  console.log(req.body);
-  res.status(201).json({
-    message: 'Thing created successfuly!',
+  const thing = new Thing({
+    title: req.body.title,
+    description: req.body.description,
+    imageUrl: req.body.imageUrl,
+    price: req.body.price,
+    userId: req.body.userId,
   });
+  thing
+    .save()
+    .then(() => {
+      res.status(201).json({
+        message: 'Post saved successfully!',
+      });
+    })
+    .catch((error) => {
+      res.status(400).json({
+        error: error,
+      });
+    });
+});
+
+app.get('/api/stuff/:id', (req, res, next) => {
+  Thing.findOne({
+    _id: req.params.id,
+  })
+    .then((thing) => {
+      res.status(200).json(thing);
+    })
+    .catch((error) => {
+      res.status(404).json({
+        error: error,
+      });
+    });
 });
 
 app.use('/api/stuff', (req, res, next) => {
-  const stuff = [
-    {
-      _id: 'aflkzhfa',
-      title: 'My first thing',
-      description: 'All of the info of my first thing',
-      imageUrl: 'https://picsum.photos/600',
-      price: 4900,
-      userId: 'kjahzfakl',
-    },
-    {
-      _id: 'aflhfa',
-      title: 'My second thing',
-      description: 'All of the info of my Second thing',
-      imageUrl: 'https://picsum.photos/600',
-      price: 1200,
-      userId: 'kjahakl',
-    },
-  ];
-
-  res.status(200).json(stuff);
+  Thing.find()
+    .then((things) => {
+      res.status(200).json(things);
+    })
+    .catch((error) => {
+      res.status(400).json({
+        error: error,
+      });
+    });
 });
 
 module.exports = app;
