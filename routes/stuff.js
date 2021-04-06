b6b5f1d1075aac5c61f334db08d51748ff99260d
khalilhamdii <khaalil.hamdi@gmail.com>
2021-04-06 01:56:39 +0100

    Modify the update function to handle update the image

--- routes/stuff.js
+++ routes/stuff.js
@@ -7,7 +7,7 @@ const multer = require('../middleware/multer-config');
 router.get('/', auth, stuffCtrl.getAllstuff);
 router.post('/', auth, multer, stuffCtrl.createThing);
 router.get('/:id', auth, stuffCtrl.getOneThing);
-router.put('/:id', auth, stuffCtrl.modifyThing);
+router.put('/:id', auth, multer, stuffCtrl.modifyThing);
 router.delete('/:id', auth, stuffCtrl.deleteThing);
 
 module.exports = router;
