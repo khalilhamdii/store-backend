e8e0e665276160f0ff99bde076235d35bb4f9d2c
khalilhamdii <khaalil.hamdi@gmail.com>
2021-04-06 01:01:20 +0100

    Add authentication middleware

--- routes/stuff.js
+++ routes/stuff.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const stuffCtrl = require('../controllers/stuff');
+const auth = require('../middleware/auth');
 
-router.get('/', stuffCtrl.getAllstuff);
-router.post('/', stuffCtrl.createThing);
-router.get('/:id', stuffCtrl.getOneThing);
-router.put('/:id', stuffCtrl.modifyThing);
-router.delete('/:id', stuffCtrl.deleteThing);
+router.get('/', auth, stuffCtrl.getAllstuff);
+router.post('/', auth, stuffCtrl.createThing);
+router.get('/:id', auth, stuffCtrl.getOneThing);
+router.put('/:id', auth, stuffCtrl.modifyThing);
+router.delete('/:id', auth, stuffCtrl.deleteThing);
 
 module.exports = router;
